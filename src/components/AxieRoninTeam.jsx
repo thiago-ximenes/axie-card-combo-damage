6cb79828755c8d952444861a9994cc1eaa77141a
Thiago Ximenes <thiagoximenes@pop-os.localdomain>
2021-12-16 19:25:59 -0300

    🚧 construction: add effect of button card of selection and remove axie from team list.

--- src/components/AxieRoninTeam.jsx
+++ src/components/AxieRoninTeam.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import { Card, Button, Col, Row } from 'react-bootstrap';
+import { useState } from 'react';
 
 export default function AxieRoninTeam(props) {
+
+  const [ select, setSelect ] = useState('Select');
+  const [ buttonVariant, setButtonVariant ] = useState('success');
 
   const { axieInfo } = props;
   const { image } = axieInfo;
 
-  console.log(axieInfo);
+  function handleClick() {
+    if (select === 'Select') {
+      setSelect('Remove');
+      setButtonVariant('danger');
+    } else {
+      setButtonVariant('success');
+      setSelect('Select');
+    }
+  }
 
   return (
     <Col lg={3} sm={1}>
@@ -14,7 +26,13 @@ export default function AxieRoninTeam(props) {
         <Card.Img variant="top" src={ image } />
         <Card.Body>
             <Row>
-              <Button variant="success">Select</Button>
+              <Button
+                size="lg"
+                variant={ buttonVariant }
+                onClick={ handleClick }
+              >
+                {select}
+              </Button>
             </Row>
         </Card.Body>
       </Card>
