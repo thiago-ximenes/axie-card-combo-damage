b6686f38004ba9a364a0afa9aaa0d87ebe3bca0b
Thiago Ximenes <thiagoximenes@pop-os.localdomain>
2021-12-20 17:56:13 -0300

    create component that show selected Axie Team while it is picking.

---
+++ src/components/AxieTeamSelected.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Card, Col, Row } from 'react-bootstrap';
+
+export default function AxieTeamSelected(props) {
+  const { axieInfo, name } = props;
+  const { image, id } = axieInfo;
+  return (
+    <Col lg={3} sm={1}>
+      <Card>
+        <Card.Img variant="top" src={ image } />
+        <Card.Body>
+          <Card.Title>
+            {name}
+          </Card.Title>
+        </Card.Body>
+      </Card>
+    </Col>
+  )
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,8 +2,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { useState, useEffect } from 'react';
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 import axieApi from '../services/getAxiesByRoninAdress';
-import { Form, Container, Button, Row, Col, InputGroup } from 'react-bootstrap';
+import { Form, Container, Button, Row, Col, InputGroup, Card } from 'react-bootstrap';
 import AxieRoninTeam from '../components/AxieRoninTeam';
+import AxieTeamSelected from '../components/AxieTeamSelected';
 import Loader from 'react-loader-spinner';
 // import { useNavigate } from 'react-router-dom';
 
@@ -52,12 +53,15 @@ export default function Home()
   }
 
   function addAxieToTeam(id) {
-    if (axieTeam.includes(id)) {
-      setAxieToTheTeam(axieTeam.filter(axie => axie !== id));
+    const axieSelected = roninDetails.find((axie) => axie.id === id);
+    if (axieTeam.includes(axieSelected)) {
+      setAxieToTheTeam(axieTeam.filter((axie) => axie !== axieSelected));
     } else if (axieTeam.length < 3) {
-    setAxieToTheTeam([ ...axieTeam, id ]);
+    setAxieToTheTeam([ ...axieTeam, axieSelected ]);
     }
   }
+
+  console.log(axieTeam)
 
   return (
     <Container className="mt-5">
@@ -92,6 +96,22 @@ export default function Home()
         </Form.Group>
       </Form>
       <h2 className="text-center">Select The Axie Team</h2>
+      <Card bg="dark">
+        <Row className='g-3 container mt-1 justify-content-md-center mb-4' xs={1} md={3}>
+          { axieTeam.length > 0 && (
+              axieTeam.map((axie) => {
+                return (
+                  <AxieTeamSelected
+                    key={ axie.id }
+                    axieInfo={ axie }
+                    teamIsFull={ teamIsFull }
+                    name={ axie.name }
+                  />
+                );
+              })
+            )}
+        </Row>
+      </Card>
       { isLoading && 
       <Loader
         type="ThreeDots"
